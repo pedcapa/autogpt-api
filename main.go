52e609d07d24bdbf05e9e60c622f5b44de8c23cf
Pedro Castañeda <catbox.py@gmail.com>
2024-07-20 00:39:18 -0600

    Fix: Set indent to 4

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"log"
+  "github.com/gofiber/fiber/v2"
+  "github.com/gofiber/fiber/v2/middleware/logger"
+  "log"
   "autogpt-api/handlers"
 )
 
 func main() {
 
-	app := fiber.New()
+  app := fiber.New()
 
-	// Middleware to log requests
-	app.Use(logger.New())
+  // Middleware to log requests
+  app.Use(logger.New())
 
-	// Routes
+  // Routes
   app.Get("/", func(c *fiber.Ctx) error {
     return c.SendString("Hello World")
   })
@@ -24,11 +24,11 @@ func main() {
   app.Get("/claude", handlers.ClaudeHandler)
   app.Get("/whisper", handlers.WhisperHandler)
 
-	// Init server
-	port := "8080"
-	err := app.Listen(":" + port)
+  // Init server
+  port := "8080"
+  err := app.Listen(":" + port)
 
-	if err != nil {
-		log.Fatal("Can't connect to port", port, ": ", err)
-	}
+  if err != nil {
+    log.Fatal("Can't connect to port", port, ": ", err)
+  }
 }
