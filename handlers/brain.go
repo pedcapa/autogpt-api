47c4ccf459d809ba46da4bbea22cf8c51af3c899
Pedro Castañeda <catbox.py@gmail.com>
2024-07-20 04:12:38 -0600

    Simple API keys check

--- handlers/brain.go
+++ handlers/brain.go
@@ -8,8 +8,27 @@ import (
 
 func OpenAIBrain(c *fiber.Ctx) error {
   OAIKey := os.Getenv("OPENAI_API_KEY")
+  GKey := os.Getenv("GEMINI_API_KEY")
+  ANTKey := os.Getenv("CLAUDE_API_KEY")
+
+  response := ""
+
   if OAIKey == "" {
-    return c.SendString("OPENAI_API_KEY is not set")
+    response += "OPENAI_API_KEY is not set"
+  } else {
+    response += fmt.Sprintf("OPENAI_API_KEY: %s", OAIKey)
   }
-  return c.SendString(fmt.Sprintf("OPENAI_API_KEY: %s", OAIKey))
+  if GKey == "" {
+    response += "\nGEMINI_API_KEY is not set"
+  } else {
+    response += fmt.Sprintf("\nGEMINI_API_KEY: %s", GKey)
+  }
+  if ANTKey == "" {
+    response += "\nCLAUDE_API_KEY is not set"
+  } else {
+    response += fmt.Sprintf("\nCLAUDE_API_KEY: %s", ANTKey)
+  }
+  
+  return c.SendString(response)
+
 }
