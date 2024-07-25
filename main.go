f2cd0e72965ac261bf6995b44485b0e11f399fed
Pedro Castañeda <catbox.py@gmail.com>
2024-07-25 00:48:26 -0600

    v1.0.2

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+  "context"
+  "log"
+  "time"
+
   "github.com/gofiber/fiber/v2"
   "github.com/gofiber/fiber/v2/middleware/logger"
-  "log"
+  "go.mongodb.org/mongo-driver/mongo"
+  "go.mongodb.org/mongo-driver/mongo/options"
+
   "autogpt-api/handlers"
   "fmt"
 )
 
-func main() {
+var userCollection *mongo.Collection
 
+func main() {
+  // MongoDB config
+  ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+  defer cancel()
+
+  clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+  client, err := mongo.Connect(ctx, clientOptions)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer client.Disconnect(ctx)
+
+  userCollection = client.Database("autogpt").Collection("users")
+
+  // Init new fiber custom app
   app := fiber.New(fiber.Config{
     Prefork: true,
     CaseSensitive: true,
     ServerHeader: "Fiber",
-    AppName: "autoGPT API v1.0.1",
+    AppName: "autoGPT API v1.0.2",
   })
   
   if !fiber.IsChild() {
@@ -25,6 +46,9 @@ func main() {
 
   // Middleware to log requests
   app.Use(logger.New())
+
+  // Pass userCollection to handlers
+  handlers.InitHandlers(userCollection)
 
   // Routes
   app.Get("/", func(c *fiber.Ctx) error {
@@ -39,7 +63,7 @@ func main() {
 
   // Init server
   port := "8080"
-  err := app.Listen(":" + port)
+  err = app.Listen(":" + port)
 
   if err != nil {
     log.Fatal("Can't connect to port", port, ": ", err)
